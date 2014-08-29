be22a3d484cc8d6b6ffc3f76aeb827a2fa6f8a5f
Eduardo Escallon <eduard.escallon@gmail.com>
2014-08-29 15:27:41 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -41,46 +41,28 @@ Application.prototype = {
 			className: "panel-success",
 			maxHeight: 180,
 			collection: new Players(),
-			tpl: "<div class='player-item'>"+
-					"<div class='player-item-container'>"+
-						"<div class='player-img'>"+
-							"<img src='"+this.server+"/image/"+this.session.getUser().image+"' width='40px' height='40px' />"+
-						"</div>"+
-						"<div class='player-info'>"+
-							"<h5><%= name %></h5>"+
-							"<div class='games-info'>"+
-								"<div class='match-info player-win'>"+
-									"<img src='images/icons/win.png' style='width:20px; margin-top:-7px'/>"+
-									"<span>2</span>"+
-								"</div>"+
-								"<div class='match-info player-loose'>"+
-									"<img src='images/icons/loose.png' style='width:20px; margin-top:-7px;'/>"+
-									"<span>3</span>"+
-								"</div>"+
+			tpl: "<div class='player-item-list'>"+
+					"<div class='player-img'>"+
+						"<img src='https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-128.png' width='40px' height='40px' />"+
+					"</div>"+
+					"<div class='player-info'>"+
+						"<h5><%= name %></h5>"+
+						"<h6><span class='glyphicon glyphicon-envelope'></span>  <%= email %></h6>"+
+						"<div class='games-info'>"+
+							"<div class='match-info your-question'>"+
+								"<span>Tu</span>"+
+								"<span>2</span>"+
+							"</div>"+
+							"<div class='match-info rival-question'>"+
+								"<span>Rival</span>"+
+								"<span>3</span>"+
+							"</div>"+
+							"<div class='match-info total-question'>"+
+								"<span>Total</span>"+
+								"<span>5</span>"+
 							"</div>"+
 						"</div>"+
-					"</div>"+
-					"<div class='player-item-vs'>"+
-						"<img src='http://www.ccbetania.org/alianzaextrema/wp-content/uploads/2012/02/VS-1.png' />"+
-					"</div>"+
-					"<div class='player-item-container player-item-container-2'>"+
-						"<div class='player-img'>"+
-							"<img src='https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-128.png' width='40px' height='40px' />"+
-						"</div>"+
-						"<div class='player-info'>"+
-							"<h5><%= name %></h5>"+
-							"<div class='games-info'>"+
-								"<div class='match-info player-win'>"+
-									"<img src='images/icons/win.png' />"+
-									"<span>2</span>"+
-								"</div>"+
-								"<div class='match-info player-loose'>"+
-									"<img src='images/icons/loose.png' />"+
-									"<span>3</span>"+
-								"</div>"+
-							"</div>"+
-						"</div>"+
-					"</div>"+
+					"</div>"+					
 				  "</div>"
 		})
 		var teamPanel = new ListPanel({
@@ -175,4 +157,49 @@ Application.prototype = {
 	},
 }
 
+
+
+
+// Template vs
+// tpl: "<div class='player-item'>"+
+// 					"<div class='player-item-container'>"+
+// 						"<div class='player-img'>"+
+// 							"<img src='"+this.server+"/image/"+this.session.getUser().image+"' width='40px' height='40px' />"+
+// 						"</div>"+
+// 						"<div class='player-info'>"+
+// 							"<h5><%= name %></h5>"+
+// 							"<div class='games-info'>"+
+// 								"<div class='match-info player-win'>"+
+// 									"<img src='images/icons/win.png' style='width:20px; margin-top:-7px'/>"+
+// 									"<span>2</span>"+
+// 								"</div>"+
+// 								"<div class='match-info player-loose'>"+
+// 									"<img src='images/icons/loose.png' style='width:20px; margin-top:-7px;'/>"+
+// 									"<span>3</span>"+
+// 								"</div>"+
+// 							"</div>"+
+// 						"</div>"+
+// 					"</div>"+
+// 					"<div class='player-item-vs'>"+
+// 						"<img src='http://www.ccbetania.org/alianzaextrema/wp-content/uploads/2012/02/VS-1.png' />"+
+// 					"</div>"+
+// 					"<div class='player-item-container player-item-container-2'>"+
+// 						"<div class='player-img'>"+
+// 							"<img src='https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-128.png' width='40px' height='40px' />"+
+// 						"</div>"+
+// 						"<div class='player-info'>"+
+// 							"<h5><%= name %></h5>"+
+// 							"<div class='games-info'>"+
+// 								"<div class='match-info player-win'>"+
+// 									"<img src='images/icons/win.png' />"+
+// 									"<span>2</span>"+
+// 								"</div>"+
+// 								"<div class='match-info player-loose'>"+
+// 									"<img src='images/icons/loose.png' />"+
+// 									"<span>3</span>"+
+// 								"</div>"+
+// 							"</div>"+
+// 						"</div>"+
+// 					"</div>"+
+// 				  "</div>"
 
