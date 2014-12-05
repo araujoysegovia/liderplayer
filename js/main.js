5b3c32c8512807edfddca486a3c9365249066699
nhernandezm <nhernandez@araujoysegovia.com>
2014-12-05 14:21:16 -0500

    pull liderplayer

--- js/main.js
+++ js/main.js
@@ -27,8 +27,8 @@ Application.prototype = {
 	router: null,
 
 	//server: "http://10.102.1.22/lider/web/app_dev.php",
-	server: "http://10.101.1.60/lider/web/app_dev.php",
-	// server: "http://localhost/lider/web/app_dev.php",
+	// server: "http://10.101.1.60/lider/web/app_dev.php",
+	server: "http://localhost/lider/web/app_dev.php",
 	//server: "http://soylider.sifinca.net",
 	// server: "http://190.242.98.189/lider/web/app_dev.php",
 	constructor: function(){
