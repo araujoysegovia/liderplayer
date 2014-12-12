b49e1253796d8bea691099b997292c030634bc9b
nhernandezm <nhernandez@araujoysegovia.com>
2014-12-12 13:14:42 -0500

    pull lider player

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ Application.prototype = {
 
 	//server: "http://10.102.1.22/lider/web/app_dev.php",
 	// server: "http://10.101.1.60/lider/web/app_dev.php",
-	server: "http://localhost/lider/web/app_dev.php",
+	server: "http://10.101.1.68/lider/web/app_dev.php",
 	//server: "http://soylider.sifinca.net",
 	// server: "http://190.242.98.189/lider/web/app_dev.php",
 	constructor: function(){
